ce2f7be2a71df224b61e9a0f851ce4e2dff8ca55
RA <numpde@null.net>
2024-02-23 16:47:02 +0300

    OpenZeppelin packages

--- code/blockchain/counter_resistance/hardhat.config.ts
+++ code/blockchain/counter_resistance/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import '@openzeppelin/hardhat-upgrades';
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
