2696eec3ca99a22fc462b93bd78ca70a9029b6f1
RA <numpde@null.net>
2024-02-29 21:35:18 +0300

    refac, docs

--- code/blockchain/counter_resistance/contracts/RegistryBase.sol
+++ code/blockchain/counter_resistance/contracts/RegistryBase.sol
@@ -12,26 +12,38 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 /// @custom:security-contact hello@counter-resistance.org
 contract RegistryBase is Initializable, ERC721Upgradeable, ERC721EnumerableUpgradeable, ERC721URIStorageUpgradeable, ERC721PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+    uint256 private _nextContributionId;
+    mapping(uint256 => address) private _originalContributor;
+
     bytes32 public constant CONTRACT_PAUSER_ROLE = keccak256("CONTRACT_PAUSER_ROLE");
     bytes32 public constant CONTRACT_UPGRADER_ROLE = keccak256("CONTRACT_UPGRADER_ROLE");
 
     bytes32 public constant CONTRIBUTOR_ROLE = keccak256("CONTRIBUTOR_ROLE");
+    bytes32 public constant CONTRIBUTOR_ROLE_MANAGER = keccak256("CONTRIBUTOR_ROLE_MANAGER");
+
     bytes32 public constant EXPERT_CONTRIBUTOR_ROLE = keccak256("EXPERT_CONTRIBUTOR_ROLE");
+    bytes32 public constant EXPERT_CONTRIBUTOR_ROLE_MANAGER = keccak256("EXPERT_CONTRIBUTOR_ROLE_MANAGER");
 
-    uint256 private _nextTokenId;
+    error NotContributor(address caller);
+    error CannotContributeForOthers(address caller, address intendedContributor);
 
-    event ContributorRoleGrantedByExpert(address indexed grantedBy, address indexed contributor);
-    event ContributorRoleRevokedByExpert(address indexed revokedBy, address indexed contributor);
+    event Contribution(address indexed by, address indexed to, uint256 indexed contributionId, string uri);
 
-    event ContributionMetadataUpdated(address indexed who, uint256 indexed contributionId);
+    event ContributionMetadataUpdated(address indexed by, uint256 indexed contributionId, string uri);
+
+    event ContributorRoleGrantedByManager(address indexed grantedBy, address indexed contributor);
+    event ContributorRoleRevokedByManager(address indexed revokedBy, address indexed contributor);
+
+    event ExpertContributorRoleGrantedByManager(address indexed grantedBy, address indexed contributor);
+    event ExpertContributorRoleRevokedByManager(address indexed revokedBy, address indexed contributor);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
         _disableInitializers();
     }
 
-    function initialize() virtual initializer public {
-        _nextTokenId = 1;
+    function initialize() public virtual initializer {
+        _nextContributionId = 1;
 
         __ERC721_init("Contribution registry", "CORE");
         __ERC721Enumerable_init();
@@ -44,7 +56,8 @@ contract RegistryBase is Initializable, ERC721Upgradeable, ERC721EnumerableUpgra
         _grantRole(CONTRACT_PAUSER_ROLE, _msgSender());
         _grantRole(CONTRACT_UPGRADER_ROLE, _msgSender());
 
-        _grantRole(EXPERT_CONTRIBUTOR_ROLE, _msgSender());
+        _grantRole(CONTRIBUTOR_ROLE_MANAGER, _msgSender());
+        _grantRole(EXPERT_CONTRIBUTOR_ROLE_MANAGER, _msgSender());
     }
 
     //
@@ -63,64 +76,140 @@ contract RegistryBase is Initializable, ERC721Upgradeable, ERC721EnumerableUpgra
     // Role management.
     //
 
-    // TODO: add grantExpertContributorRole
+    /**
+     * @dev Grants the EXPERT_CONTRIBUTOR_ROLE, whether present or not.
+     * Can only be called by an account with the EXPERT_CONTRIBUTOR_ROLE_MANAGER.
+     * @param expertContributor Address of the new expert contributor to be granted the role.
+     */
+    function grantExpertContributorRole(address expertContributor) public onlyRole(EXPERT_CONTRIBUTOR_ROLE_MANAGER) {
+        _grantRole(EXPERT_CONTRIBUTOR_ROLE, expertContributor);
+
+        emit ExpertContributorRoleGrantedByManager(_msgSender(), expertContributor);
+    }
 
     /**
-     * @dev Grants the CONTRIBUTOR_ROLE to a new contributor.
-     * Can only be called by an account with the EXPERT_CONTRIBUTOR_ROLE.
+     * @dev Revokes the EXPERT_CONTRIBUTOR_ROLE, whether present or not.
+     * Can only be called by an account with the EXPERT_CONTRIBUTOR_ROLE_MANAGER.
+     * @param expertContributor Address of the expert contributor to have the role revoked.
+     */
+    function revokeExpertContributorRole(address expertContributor) public onlyRole(EXPERT_CONTRIBUTOR_ROLE_MANAGER) {
+        _revokeRole(EXPERT_CONTRIBUTOR_ROLE, expertContributor);
+
+        emit ExpertContributorRoleRevokedByManager(_msgSender(), expertContributor);
+    }
+
+    /**
+     * @dev Grants the CONTRIBUTOR_ROLE, whether present or not.
+     * Can only be called by an account with the CONTRIBUTOR_ROLE_MANAGER.
      * @param contributor Address of the new contributor to be granted the role.
      */
-    function grantContributorRole(address contributor) public onlyRole(EXPERT_CONTRIBUTOR_ROLE) {
+    function grantContributorRole(address contributor) public onlyRole(CONTRIBUTOR_ROLE_MANAGER) {
         _grantRole(CONTRIBUTOR_ROLE, contributor);  // Does not emit if already granted
 
-        emit ContributorRoleGrantedByExpert(_msgSender(), contributor);
+        emit ContributorRoleGrantedByManager(_msgSender(), contributor);
     }
 
     /**
-     * @dev Revokes the CONTRIBUTOR_ROLE from an existing contributor.
-     * Can only be called by an account with the EXPERT_CONTRIBUTOR_ROLE.
+     * @dev Revokes the CONTRIBUTOR_ROLE, whether present or not.
+     * Can only be called by an account with the CONTRIBUTOR_ROLE_MANAGER.
      * @param contributor Address of the contributor to be revoked the role.
      */
-    function revokeContributorRole(address contributor) public onlyRole(EXPERT_CONTRIBUTOR_ROLE) {
+    function revokeContributorRole(address contributor) public onlyRole(CONTRIBUTOR_ROLE_MANAGER) {
         _revokeRole(CONTRIBUTOR_ROLE, contributor);  // Does not emit if already revoked
 
-        emit ContributorRoleRevokedByExpert(_msgSender(), contributor);
+        emit ContributorRoleRevokedByManager(_msgSender(), contributor);
     }
 
     //
-    // Contribution.
+    // Data contribution (i.e., minting).
     //
 
-    function contributeFor(address to, string memory uri)
-    public
+    /**
+     * @dev Internal function to handle contributions. It checks if the caller has the appropriate role
+     * to contribute on behalf of another address or for themselves, then "mints" a new contribution.
+     * This function can be called by anyone with the EXPERT_CONTRIBUTOR_ROLE to contribute for another address,
+     * or by those with the CONTRIBUTOR_ROLE to contribute for themselves.
+     * @param to The address for whom the contribution is being made.
+     * @param uri The URI for the contribution metadata.
+     * @return contributionId The ID of the newly minted contribution.
+     */
+    function _contribute(address to, string memory uri)
+    internal
     whenNotPaused
-    onlyRole(EXPERT_CONTRIBUTOR_ROLE)
-    returns (uint256)
+    returns (uint256 contributionId)
     {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        bool isExpert = hasRole(EXPERT_CONTRIBUTOR_ROLE, _msgSender());
+        bool isContributor = hasRole(CONTRIBUTOR_ROLE, _msgSender());
 
-        return tokenId;
+        if (!isExpert && !isContributor) {
+            revert NotContributor(_msgSender());
+        }
+
+        if (isContributor && (to != _msgSender())) {
+            revert CannotContributeForOthers(_msgSender(), to);
+        }
+
+        contributionId = _nextContributionId++;
+
+        _safeMint(to, contributionId);
+        _setTokenURI(contributionId, uri);
+
+        _originalContributor[contributionId] = _msgSender();
+
+        emit Contribution(_msgSender(), to, contributionId, uri);
+
+        return contributionId;
     }
 
-    function contribute(string memory uri)
-    public
-    whenNotPaused
-    onlyRole(CONTRIBUTOR_ROLE)
-    returns (uint256)
-    {
-        uint256 tokenId = _nextTokenId++;
-        _safeMint(_msgSender(), tokenId);
-        _setTokenURI(tokenId, uri);
+    /**
+     * @dev Allows a user with the EXPERT_CONTRIBUTOR_ROLE to make a contribution on behalf of another address.
+     * @param to The address for whom the contribution is being made.
+     * @param uri The URI for the contribution metadata.
+     * @return contributionId The ID of the new contribution.
+     */
+    function contributeFor(address to, string memory uri) public virtual returns (uint256 contributionId) {
+        contributionId = _contribute(to, uri);
+    }
 
-        return tokenId;
+    /**
+     * @dev Allows any user with the CONTRIBUTOR_ROLE to make a contribution for themselves.
+     * @param uri The URI for the contribution metadata.
+     * @return contributionId The ID of the new contribution.
+     */
+    function contribute(string memory uri) public virtual returns (uint256 contributionId) {
+        contributionId = _contribute(_msgSender(), uri);
+    }
+
+    /**
+     * @dev Returns the original contributor of a specific contribution (even if it has been "burned").
+     * If the contribution ID does not exist, returns the zero address.
+     *
+     * @param contributionId The ID of the contribution.
+     * @return The address of the original contributor, or the zero address if ID does not exist.
+     */
+    function getOriginalContributor(uint256 contributionId) public view returns (address) {
+        return _originalContributor[contributionId];
     }
 
     //
     // Contribution metadata.
     //
 
+    /**
+     * @dev Updates the URI for a specific contribution. This function allows the contribution's
+     * metadata URI to be updated, but only by users with the necessary permissions. Expert
+     * contributors can update the URI for any contribution, while regular contributors can only
+     * update URIs for contributions they own. The function checks for the existence of the
+     * contribution by attempting to retrieve its owner, which will revert if the contribution
+     * does not exist.
+     *
+     * Requirements:
+     * - The contract must not be paused.
+     * - The caller must be an expert contributor or the owner of the contribution (for regular contributors).
+     *
+     * @param contributionId The ID of the contribution whose URI is being updated.
+     * @param uri The new URI string to be associated with the contribution.
+     */
     function setContributionURI(uint256 contributionId, string memory uri) public whenNotPaused {
         // Attempt to get the owner of the contribution to implicitly check if it exists.
         // This will revert if the contribution does not exist.
@@ -134,8 +223,9 @@ contract RegistryBase is Initializable, ERC721Upgradeable, ERC721EnumerableUpgra
 
         _setTokenURI(contributionId, uri);
 
-        emit ContributionMetadataUpdated(_msgSender(), contributionId);
+        emit ContributionMetadataUpdated(_msgSender(), contributionId, uri);
     }
+
 
     /**
      * @dev Preferred method for fetching contribution URIs, acting as a wrapper to {tokenURI}.
