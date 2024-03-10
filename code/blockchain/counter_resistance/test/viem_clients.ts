c8a5f96b6dd6073df3110a4b68d3282fe3aaa1cf
RA <numpde@null.net>
2024-03-10 09:04:46 +0300

    rm viem util

--- code/blockchain/counter_resistance/test/viem_clients.ts
+++
@@ -1,9 +0,0 @@
-import hre from "hardhat";
-import type {WalletClient} from "@nomicfoundation/hardhat-viem/types";
-
-export async function getClients(): Promise<{ [key: string]: WalletClient }> {
-    const [, deployer, admin, upgrader, pauser, paymaster, manager, shop1, shop2, customer1, customer2, third]:
-        WalletClient[] = await hre.viem.getWalletClients();
-
-    return {deployer, admin, upgrader, pauser, paymaster, manager, shop1, shop2, customer1, customer2, third};
-}
