2e7aeeedb96b8b7ead959b6f54f84f8759de3604
RA <numpde@null.net>
2024-03-12 14:12:20 +0300

    added `companion`

--- code/blockchain/counter_resistance/test/utils/ethers_signers.ts
+++ code/blockchain/counter_resistance/test/utils/ethers_signers.ts
@@ -4,12 +4,12 @@ import {SignerWithAddress} from "@nomicfoundation/hardhat-ethers/signers";
 export async function getSigners(): Promise<{ [key: string]: SignerWithAddress }> {
     const [
         ,
-        deployer, admin, upgrader, pauser, paymaster, manager, contributor, expertContributor, nonContributor, third
+        deployer, admin, companion, upgrader, pauser, paymaster, manager, contributor, expertContributor, nonContributor, third
     ]
         : SignerWithAddress[]
         = await ethers.getSigners();
 
     return {
-        deployer, admin, upgrader, pauser, paymaster, manager, contributor, expertContributor, nonContributor, third
+        deployer, admin, companion, upgrader, pauser, paymaster, manager, contributor, expertContributor, nonContributor, third
     };
 }
